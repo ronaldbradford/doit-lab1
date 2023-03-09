70e975825f90e4e4e36b33cc885b24b3eaaddfb6
Ronald Bradford <ronald@doit-intl.com>
2023-03-09 08:19:24 -0500

    rework random load generator

--- lesson3/src/test/emulate_load.py
+++ lesson3/src/test/emulate_load.py
@@ -10,21 +10,16 @@ class Telemetry(HttpUser):
     def send(self):
         num = round(np.random.uniform(100))
         for iterations in range(int(num/14)):
-          memory_usage = round(np.random.default_rng().uniform(0,num/100),2)
-          cpu_usage = round(np.random.random(),2)
-          if (int(iterations/2)) == 0:
-            cpu_usage *= round(np.random.random(),2)
-            memory_usage = abs(memory_usage - cpu_usage)
-            if (cpu_usage > 0.8):
-              cpu_usage -= 0.21
-            if (cpu_usage < memory_usage):
-              memory_usage = abs(memory_usage - (num/12)*.1)
-          if cpu_usage < 0.15:
-            cpu_usage += (num/15)*.1
-          num += round(np.random.uniform(100))*round(np.random.uniform(6))
-          self.client.post("/telemetry", json={
-              "timestamp": f'{datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]}Z',
-              "device_id": f"abc{num}",
-              "memory_usage": memory_usage,
-              "cpu_usage": cpu_usage
-          })
+            memory_usage = round(np.random.default_rng().uniform(0,num/100),2)
+            cpu_usage = round(np.random.random(),2)
+            if (int(iterations/2)) == 0:
+                num += round(np.random.uniform(100))*round(np.random.uniform(6))
+            if int(num/9) == 0:
+                cpu_usage -= 0.21
+
+            self.client.post("/telemetry", json={
+                "timestamp": f'{datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]}Z',
+                "device_id": f"abc{num}",
+                "memory_usage": memory_usage,
+                "cpu_usage": cpu_usage
+            })
