721820bf52e53730c18599fcbe474ee04b1eb80e
Ronald Bradford <ronald@doit-intl.com>
2023-03-09 08:31:00 -0500

    rework random load generator

--- lesson3/src/test/emulate_load.py
+++ lesson3/src/test/emulate_load.py
@@ -10,12 +10,19 @@ class Telemetry(HttpUser):
     def send(self):
         num = round(np.random.uniform(100))
         for iterations in range(int(num/14)):
-            memory_usage = round(np.random.default_rng().uniform(0,num/100),2)
+            memory_usage = round(np.random.default_rng().uniform(0,1),2)
             cpu_usage = round(np.random.random(),2)
-            if (int(iterations/2)) == 0:
+            if (int(iterations/4)) == 0:
                 num += round(np.random.uniform(100))*round(np.random.uniform(6))
-            if int(num/9) == 0:
+                memory_usage /= 2
+            if int(num/9) == 0 and cpu_usage > 0.25:
                 cpu_usage -= 0.21
+            if int(num/2) == 0 and cpu_usage > 0.85:
+                cpu_usage -= 0.06
+            if int(num/10) == 0 and cpu_usage < 0.10:
+                cpu_usage += 0.16
+            if int(num/7) == 0 and memory_usage < 0.45:
+                memory_usage *= 2
 
             self.client.post("/telemetry", json={
                 "timestamp": f'{datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]}Z',
